234294d3f534f27981b0768aa811fcfdd4fd5cdb
Hernán Felipe Castro <hernan.castro.j@gmail.com>
2017-09-25 17:11:53 -0300

    proyecto terminado

--- public/js/app.js
+++ public/js/app.js
@@ -39,9 +39,7 @@ function setForm() {
     dataType: 'text',
     type: 'GET',
     success: function(datos) {
-      var dat = JSON.parse(datos)
-      console.log(dat)
-
+      var dat = JSON.parse(datos);
       var lugares = [];
       var tipos = [];
       var precios = [];
@@ -66,12 +64,12 @@ function setForm() {
 
 
       $.each(lugares_unicos, function(i, item) {
-        var item_ = item.replace(/ /g, '_');
+        var item_ = item.replace(/ /g, '');
         $('#ciudad').append('<option value=' + item_ + '>' + item + '</option>')
       });
 
       $.each(tipos_unicos, function(i, item) {
-        var item_ = item.replace(/ /g, '_');
+        var item_ = item.replace(/ /g, '');
         $('#tipo').append('<option value=' + item_ + '>' + item + '</option>')
       });
 
@@ -98,6 +96,8 @@ $("#buscar").click(function() {
   var precio_min = value[0];
   var precio_max = value[1];
 
+  $('#lista').empty()
+
   $.ajax({
     url: "buscar",
     dataType: 'text',
@@ -105,19 +105,78 @@ $("#buscar").click(function() {
     success: function(datos) {
 
       var dat = JSON.parse(datos)
-      console.log(dat)
-      console.log(ciudad)
-      console.log(tipo)
-      console.log(precio_min)
-      console.log(precio_max)
+      var dat_f_c = [];
+      var dat_f = [];
+      var dat_f_t = [];
 
-     $.each(dat, function(i, item) {
+      if ($('#personalizada').hasClass('invisible')) {
 
+        dat_f = dat;
 
+      } else {
 
-     });
+        $.each(dat, function(i, item) {
+          var $this = dat[i];
+          var ciudad_ = item.Ciudad.replace(/ /g, '');
+          if (ciudad != null) {
 
+            if (ciudad_.toUpperCase() == ciudad.toUpperCase()) {
+              dat_f_c.push($this)
+            };
+          } else {
+            dat_f_c.push($this)
+          };
+        });
 
+        $.each(dat_f_c, function(i, item) {
+          var $this = dat_f_c[i];
+          var tipo_ = item.Tipo.replace(/ /g, '');
+          if (tipo != null) {
+            if (tipo_.toUpperCase() == tipo.toUpperCase()) {
+              dat_f_t.push($this)
+            };
+          } else {
+            dat_f_t.push($this)
+          };
+        });
+
+        $.each(dat_f_t, function(i, item) {
+          var $this = dat_f_t[i];
+          var precio = item.Precio;
+          var long = precio.length;
+          var res = precio.slice(1, long);
+          precio_ = res.replace(/,/g, '');
+          var precio_x = Number(precio_)
+
+          if (precio_min <= precio_x && precio_max >= precio_x) {
+            dat_f.push($this)
+          };
+        });
+      };
+
+      $.each(dat_f, function(i, item) {
+
+        var propiedad_template = '<div class="card horizontal">' +
+          '<div class="card-image"><img src=":image:"></div>' +
+          '<div class="card-stacked"><div class="card-content">' +
+          '<div><b>Direccion: </b><p style="display:inline">:direccion:</p></div>' +
+          '<div><b>Ciudad: </b><p style="display:inline">:ciudad:</p></div>' +
+          '<div><b>Telefono: </b><p style="display:inline">:telefono:</p></div>' +
+          '<div><b>Código postal: </b><p style="display:inline">:codigo:</p></div>' +
+          '<div><b>Precio: </b><p style="display:inline">:precio:</p></div>' +
+          '<div><b>Tipo: </b><p style="display:inline">:tipo:</p></div></div></div></div>'
+
+        var new_prop = propiedad_template.replace(':image:', 'img/home.jpg')
+          .replace(':direccion:', item.Direccion)
+          .replace(':ciudad:', item.Ciudad)
+          .replace(':telefono:', item.Telefono)
+          .replace(':codigo:', item.Codigo_Postal)
+          .replace(':precio:', item.Precio)
+          .replace(':tipo:', item.Tipo)
+
+        $('#lista').append(new_prop);
+
+      });
 
 
     }
